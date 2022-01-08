70d5c14741ff8aa88f3858dbe2977d283c74cb41
eric ogbe <tradelearn13@gmail.com>
2022-01-08 15:34:46 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+const twitterCount = document.querySelector('.twitter-count')
+const instaCount = document.querySelector('.insta-count')
+const youtubeCount = document.querySelector('.youtube-count')
+
+
+let twitter = 0;
+let insta = 0;
+let youtube = 0;
+
+function twitterCounter() {
+   if ( twitter > 204){
+       twitter = 205
+   }else{
+       twitter++
+   }
+   
+    twitterCount.innerHTML = twitter
+}
+
+setInterval(twitterCounter, 2)
+
+function youtubeCounter() {
+    if ( youtube > 303){
+        youtube = 304
+    }else{
+        youtube++
+    }
+    
+     youtubeCount.innerHTML = youtube
+ }
+ 
+ setInterval(youtubeCounter, 1.5)
+
+ function instaCounter() {
+    if ( insta > 401){
+        insta = 402
+    }else{
+        insta++
+    }
+    
+     instaCount.innerHTML = insta
+ }
+ 
+ setInterval(instaCounter, 1)
+    
+
+
+ 
+        
